fd59137905b96e762be1c328feef4832f659d9c4
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-01 10:33:07 +0200

    email validation added

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-function check() {
-    var email_x = document.querySelector(".email").value;
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!filter.test(email.value)) {
-        alert('Please provide a valid email address');
-        email.focus;
-        return false;
+const notifyBTN = document.querySelector('button.notify-btn');
+const error = document.querySelector('.error');
+const emailInput = document.querySelector('input.email');
+const myForm = document.querySelector('form');
+
+notifyBTN.addEventListener('click', validation);
+let validError = false;
+
+function validation(e) {
+
+    e.preventDefault();
+    error.classList.add('hide');
+    emailInput.style.borderColor = '#ddd';
+    if (emailInput.value.length == 0) {
+        addError(emailInput, 'Required Field');
     } else {
-        alert("Thanks for your intrest in us, Now you  will be able to receive monthly updates from us.  ");
-        document.querySelector('.email').value = "";
+        let exp = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        let result = exp.test(emailInput.value);
+        if (!result) {
+            addError(emailInput, 'Please provide a valid email address');
+            validError = true;
+        } else {
+            validError = false;
+            console.log(validError);
+        }
     }
+    if (!validError) {
+        console.log("Submit");
+        myForm.submit();
+    }
+
+}
+
+function addError(input, mes) {
+    let temp = input.nextElementSibling;
+    temp.classList.remove('hide');
+    temp.textContent = mes;
+    input.style.borderColor = 'red';
+    input.focus();
 }
